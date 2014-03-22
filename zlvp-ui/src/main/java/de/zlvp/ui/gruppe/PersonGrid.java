ce0f9d1b1c97200c00200f3c3caf3479768fb2a8
Waldi Waldemar <waldi1985@gmx.de>
2014-03-22 22:32:29 +0100

    person grid erweitert

--- zlvp-ui/src/main/java/de/zlvp/ui/gruppe/PersonGrid.java
+++ zlvp-ui/src/main/java/de/zlvp/ui/gruppe/PersonGrid.java
@@ -8,19 +8,26 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiFactory;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.data.client.loader.RpcProxy;
 import com.sencha.gxt.data.shared.ListStore;
-import com.sencha.gxt.widget.core.client.Composite;
+import com.sencha.gxt.data.shared.loader.LoadResultListStoreBinding;
+import com.sencha.gxt.data.shared.loader.PagingLoadConfig;
+import com.sencha.gxt.data.shared.loader.PagingLoadResult;
+import com.sencha.gxt.data.shared.loader.PagingLoader;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.Grid;
 import com.sencha.gxt.widget.core.client.grid.GroupingView;
+import com.sencha.gxt.widget.core.client.toolbar.PagingToolBar;
 
 import de.zlvp.model.Gruppe;
 import de.zlvp.model.Person;
 import de.zlvp.model.Teilnehmer;
 
-public class PersonGrid extends Composite {
+public class PersonGrid implements IsWidget {
 
 	private static PersonGridUiBinder uiBinder = GWT.create(PersonGridUiBinder.class);
 
@@ -43,6 +50,9 @@ public class PersonGrid extends Composite {
 	ColumnConfig<Person, Date> geburtsdatum = new ColumnConfig<Person, Date>(properties.geburtsdatum());
 
 	@UiField
+	PagingToolBar toolBar;
+
+	@UiField
 	ColumnModel<Person> cm;
 
 	@UiField(provided = true)
@@ -54,6 +64,9 @@ public class PersonGrid extends Composite {
 	@UiField
 	Grid<Person> grid;
 
+	@UiField(provided = true)
+	PagingLoader<PagingLoadConfig, PagingLoadResult<Person>> loader;
+
 	public PersonGrid() {
 		name.setHeader("Nachname");
 		vorname.setHeader("Vorname");
@@ -61,7 +74,49 @@ public class PersonGrid extends Composite {
 		plz.setHeader("PLZ");
 		ort.setHeader("Ort");
 		geburtsdatum.setHeader("Geburtsdatum");
-		initWidget(uiBinder.createAndBindUi(this));
+
+		RpcProxy<PagingLoadConfig, PagingLoadResult<Person>> proxy = new RpcProxy<PagingLoadConfig, PagingLoadResult<Person>>() {
+			@Override
+			public void load(final PagingLoadConfig loadConfig, AsyncCallback<PagingLoadResult<Person>> callback) {
+				callback.onSuccess(new PagingLoadResult<Person>() {
+
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public List<Person> getData() {
+						// TODO Auto-generated method stub
+						return null;
+					}
+
+					@Override
+					public int getOffset() {
+						// TODO Auto-generated method stub
+						return loadConfig.getOffset();
+					}
+
+					@Override
+					public int getTotalLength() {
+						// TODO Auto-generated method stub
+						return 0;
+					}
+
+					@Override
+					public void setOffset(int offset) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void setTotalLength(int totalLength) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+			}
+		};
+		loader = new PagingLoader<PagingLoadConfig, PagingLoadResult<Person>>(proxy);
+		loader.setRemoteSort(true);
+		loader.addLoadHandler(new LoadResultListStoreBinding<PagingLoadConfig, Person, PagingLoadResult<Person>>(store));
 	}
 
 	public void setGruppe(Gruppe gruppe) {
@@ -82,5 +137,12 @@ public class PersonGrid extends Composite {
 		l.add(geburtsdatum);
 
 		return new ColumnModel<Person>(l);
+	}
+
+	@Override
+	public Widget asWidget() {
+		Widget widget = uiBinder.createAndBindUi(this);
+		toolBar.bind(loader);
+		return widget;
 	}
 }
